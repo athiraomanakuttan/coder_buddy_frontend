2f79488fc8b45ea5be68c7822122c9142d26d722
athira <athiraomanakuttan20@gmail.com>
2025-03-06 00:31:19 +0530

    fix(minor changes): some minor changes in several pages

--- src/app/services/expert/paymentApi.ts
+++ src/app/services/expert/paymentApi.ts
@@ -22,7 +22,6 @@ export const getExpertWalletData = async (token: string)=>{
         return responce.data
     } catch (error) {
         console.log(error)
-        toast.error("unable to fetch the wallet");
     }
 }
 
